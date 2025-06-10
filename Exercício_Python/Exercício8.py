0dc532a0b22291fe28cdc4646293575a748df596
RaimundaLacerda <nonatalacerda@gmail.com>
2025-06-10 20:30:09 -0300

    Exercício oito corrigido

--- Exercício_Python/Exercício8.py
+++ Exercício_Python/Exercício8.py
@@ -1,4 +1,13 @@
-resultado = soma(2, 3)
+# resultado = soma(2, 3)
+
+# def soma(a, b):
+#     return a + b
+
+#Correção
 
 def soma(a, b):
     return a + b
+
+resultado = soma(2, 3)
+print(resultado)  
+
