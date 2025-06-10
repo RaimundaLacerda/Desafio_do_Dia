894be8eaaa25be7fa2d0deee2244e72bc378e2fe
RaimundaLacerda <nonatalacerda@gmail.com>
2025-06-10 20:26:29 -0300

    Exercício sete corrigido

--- Exercício_Python/Exercício7.py
+++ Exercício_Python/Exercício7.py
@@ -1,3 +1,10 @@
+# numero = 10
+# if numero < 5 and numero > 20:
+#     print("Número válido")
+
+#Correção
+
 numero = 10
-if numero < 5 and numero > 20:
+if numero > 5 and numero < 20:
     print("Número válido")
+
